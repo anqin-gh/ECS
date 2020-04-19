0e6f45cd46200d72f3fbea82b14f9f3ddb186b1a
anqin <anqin2@protonmail.com>
2020-04-19 11:34:10 +0200

    Keep constant FPS in main loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+#include <chrono>
+#include <thread>
+
 #include <cstdint>
 #include <cstdio>
 
@@ -13,21 +16,34 @@ extern "C" {
 constexpr uint32_t kSCRWIDTH  { 640 };
 constexpr uint32_t kSCRHEIGHT { 360 };
 
+using namespace std::chrono_literals;
+constexpr auto kSPF { 1ms };
+
 int main() {
     try {
+        // Entities
         ECS::EntityManager_t entityMan;
         entityMan.createEntity( 20, 40, "assets/jerry.png");
         entityMan.createEntity( 80, 10, "assets/jerry.png");
         auto& e = entityMan.createEntity(200, 170, "assets/jerry.png");
         entityMan.addInputComponent(e);
+
+        // Systems
         const ECS::RenderSystem_t render{kSCRWIDTH, kSCRHEIGHT};
         ECS::PhysicsSystem_t physics;
         ECS::CollisionSystem_t collision;
         ECS::InputSystem_t input;
+
+        using clk = std::chrono::steady_clock;
+        // main loop
         while(render.update(entityMan)) {
+            auto lastTime = clk::now();
             input.update(entityMan);
             physics.update(entityMan);
             collision.update(entityMan);
+            auto elapsed = clk::now() - lastTime;
+            if(elapsed < kSPF)
+                std::this_thread::sleep_for( kSPF - elapsed );
         }
     } catch(...) {
         std::puts("Capturada");
